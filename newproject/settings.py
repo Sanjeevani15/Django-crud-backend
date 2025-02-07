a7431ac2970bd1ee868ae66e9ee0dd4366cbaad8
Sanjeevani15 <sanjeevanibhandari3@gmail.com>
2025-02-07 15:49:36 +0530

    new

--- newproject/settings.py
+++ newproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)29su@a1=s2@1u@^3%jq!-me&dj00d53&z1%czij%&z_r1hkrk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
